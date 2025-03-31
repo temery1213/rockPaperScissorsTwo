a9bfac4a299022856849342064824f278d378ba1
temery1213 <temery1213@gmail.com>
2025-03-31 12:02:21 -0500

    added function for playround added function to play an entire game (iterates play round 5 times)

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
-let computerChoice = getComputerChoice();
-let playerChoice = getPlayerChoice();
+
 
     function getComputerChoice() {
     let randomNumber = (Math.random() * 100);
@@ -17,15 +16,68 @@ let playerChoice = getPlayerChoice();
     function getPlayerChoice() {
         let choice = prompt("Choose wisely...")
         return choice.toLowerCase();
-    }
+ }
+
 
     function playRound(playerChoice, computerChoice) {
-        if (playerChoice === rock && computerChoice === paper)
+
+        if (playerChoice === "rock" && computerChoice === "scissors") {
             playerScore++;
-            return "You win the round!"
+            console.log("You win the round!")
+            return;
+         } else if (playerChoice === "rock" && computerChoice === "paper") {
+            computerScore++;
+            console.log("You lose the round!");
+            return;
+        } else if (playerChoice === "rock" && computerChoice === "rock") {
+            console.log("It's a tie!")
+            return;
+         
+        } else if (playerChoice === "paper" && computerChoice === "scissors") {
+            computerScore++;
+            console.log("You lose the round!");
+            return;
+         } else if (playerChoice === "paper" && computerChoice === "paper") {
+            console.log("It's a tie!");
+            return;
+         } else if (playerChoice === "paper" && computerChoice === "rock") {
+            playerScore++;
+            console.log("You win the round!");
+            return;
+    
+       
+        } else if (playerChoice === "scissors" && computerChoice === "scissors") {
+            return "It's a tie!";
+        } else if (playerChoice === "scissors" && computerChoice === "paper") {
+            playerScore++;
+            console.log("You win the round!");
+            return;
+        } else if (playerChoice === "scissors" && computerChoice === "rock") {
+            computerScore++;
+            console.log("You lose the round!");
+            return;
+   }
+}
+
+    function playGame() {
+       
+        for ( let i = 0; i < 5; i++) {
+            
+            let playerChoice = getPlayerChoice();
+            let computerChoice = getComputerChoice();
+            console.log("player: " + playerChoice);
+            console.log("computer: " + computerChoice);
+            playRound(playerChoice, computerChoice);
+            console.log("Player score: " + playerScore)
+            console.log("Computer score: " + computerScore)
+            
+        }
+        if (playerScore > computerScore) {
+            console.log("You win the game!")
+        } else {
+            console.log("You lose the game!")
+        }
     }
 
-
-
-console.log("player: " + playerChoice);
-console.log("computer: " + computerChoice);
+    playGame();
+    
